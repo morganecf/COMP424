74afea3d4970432cc02ef1b949f4fc7119f95424
Morgane Ciot <morganeciot@Morganes-MacBook-Air.local>
2013-03-17 00:33:02 -0400

    function descriptions

--- src/COMP421PoliceDB.java
+++ src/COMP421PoliceDB.java
@@ -25,15 +25,44 @@ public class COMP421PoliceDB {
 		{
 			System.out.println("Please select one of the following options by typing the associated number:\n");
 			System.out.println("1. Test query");
-			System.out.println("6. Quit\n");
+			System.out.println("2. Test query");
+			System.out.println("3. Test query");
+			System.out.println("4. Test query");
+			System.out.println("5. Test query");
+			System.out.println("6. Test query");
+			System.out.println("7. Quit\n");
 			int userin = getUserChoice();
 			if (userin == 1)
 			{
-				optionOne();
+				isCriminal();
+			}
+			else if (userin == 2)
+			{
+				addOffense();
+			}
+			else if (userin == 3)
+			{
+				increaseSalary();
+			}
+			else if (userin == 4)
+			{
+				updatePopulation();
+			}
+			else if (userin == 5)
+			{
+				runStats();
 			}
 			else if (userin == 6)
 			{
+				optimize();
+			}
+			else if (userin == 7)
+			{
 				run = false;
+			}
+			else if (userin == 8) 
+			{
+				testRun();
 			}
 			else if (userin == -1)
 			{
@@ -67,7 +96,52 @@ public class COMP421PoliceDB {
 		}
 	}
 	
-	public static void optionOne()
+	// Look up whether a given person is a criminal
+	// If criminal exists return their criminal record
+	public static void isCriminal() {
+		return;
+	}
+	
+	// Add an offense to the database
+	// Prompts user if adding a traffic violation or crime
+	// Prompts user for all other info (criminal name/location of crime/etc...)
+	// If traffic violation and occurred 5 years ago, don't add 
+	// If crime and occurred 10 years ago, don't add but record in log file 
+	// Could these be triggers? 
+	// can use isCriminal() to check if already exists
+	public static void addOffense() {
+		return;
+	}
+	
+	// Increase the salary of all police officers with ranking >= x or function = 's'
+	// Increase by a certain percentage based on the crime rate
+	// Prompt user for ranking vs. function, id (or name??), percentage
+	// Have error handling or triggers if combined salary exceeds budget 
+	public static void increaseSalary() {
+		return;
+	}
+	
+	// Update the population with new census information 
+	// Asks for the new population of each borough
+	// If the crime rate changes by a certain amount then ask user if want to reallocate, then run analyze() ??
+	public static void updatePopulation() {
+		return;
+	}
+	
+	// Compile statistics
+	// Ex: boroughs with highest crime rate, intersections with high volume of traffic violations, most criminal gender, etc.
+	// Could have option to run all, or specify which borough you want to see stats for (for example)
+	public static void runStats() {
+		return;
+	}
+	
+	// Optimization algorithm - efficient allocation of salary/police officers based on crime rate and crime types
+	// Should have modification option 
+	public static void optimize() {
+		return;
+	}
+	
+	public static void testRun()
 	{
 		ResultSet test = db.runQuery("SELECT * FROM Offender");
 		if (test != null)
@@ -94,8 +168,8 @@ public class COMP421PoliceDB {
 				System.out.println("No results found.");
 			}
 		}
-		
-		
 	}
+	
+	public static void 
 
 }
